6d6d5160d679c3ef21157a0d9d16db81ad02b725
alumno <alumno>
2019-03-01 12:27:20 -0300

    clase frustracion jhe

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
@@ -1,3 +1,13 @@
+/*Realizar el algoritmo que permita iterar el ingreso de dos datos, un país y la
+superficie de su territorio (validar) hasta que el usuario quiera e informar al terminar
+el ingreso por document.write:
+1-La cantidad de países con superficie impar.
+2-La cantidad de países con superficie menor a 100
+3-La cantidad de países con superficie igual a 100
+4-El nombre del primer país que superó los 100 de superficie
+5-El promedio de kilómetros ingresados.
+6-El nombre del que menos territorio tiene.*/ 
+
 function mostrar()
 {
 
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
@@ -1,4 +1,55 @@
-function mostrar()
-{
+/*Realizar el algoritmo que permita el ingreso por prompt de las velocidades en
+kilómetros (validar entre 0 y 250) , el tipo de combustible, validar el tipo “s” o “l”
+para solido o liquido, de 5 vehículos de prueba ,informar por alert:
+1-El promedio de las velocidades totales.
+2-La velocidad más baja y el tipo de combustible de ese vehículo.
+3-La cantidad de combustibles líquidos que su velocidad supere los 100
+kilómetros.
+4-la velocidad más alta de los de combustible sólido*/
 
+function mostrar()
+{   
+    var pruebas;
+    var velocidadIngresada;
+    var totalVelocidades;
+    var tipoCombustible;
+    var tipoCombustibleMasBajo;
+    var velocidadMasBaja;
+    totalVelocidades = 0;
+    pruebas = 0;
+    velocidadMasBaja = 0;
+
+
+
+    while(pruebas <5)
+    {
+        
+    velocidadIngresada = prompt ("Ingrese la velocidad del vehiculo, debe ser entre 0 y 250");
+    if(velocidadIngresada >= 0 || velocidadIngresada <= 250)
+    
+    {
+        totalVelocidades = parseInt(totalVelocidades) + parseInt(velocidadIngresada);
+    
+    }
+    else {
+        velocidadIngresada = prompt ("Ingrese la velocidad del vehiculo, debe ser entre 0 y 250");
+        
+    }
+    tipoCombustible = prompt ("Ingrese el tipo de combustible, siendo S para solido y L para liquido")
+
+    //console.log (velocidadIngresada)
+     if(velocidadIngresada < velocidadMasBaja || pruebas == 0){
+         velocidadMasBaja = velocidadIngresada;
+         tipoCombustibleMasBajo = tipoCombustible;
+    }
+    
+    pruebas++;
+    }
+    
+    //tipoCombustible = prompt("Ingrese el tipo de combustible siendo L para liquido y S para solido"); 
+    
+    alert ("El promedio de las velocidad es  = "+ (totalVelocidades/5))
+    alert ("La velocidad mas baja es   = "+ velocidadMasBaja)
 }
+    
+
